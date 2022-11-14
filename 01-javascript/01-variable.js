f9c6942e60fb025d1c98b8acfe0807f086133d9a
Jenny Llano <jenny.llano@epn.edu.ec>
2022-11-14 17:58:37 -0500

    hasta variables

---
+++ 01-javascript/01-variable.js
@@ -0,0 +1,32 @@
+//01 javascript
+//01-variable.js
+
+//mutables (re asignadas)
+var numeroUno = 1;
+let numeroDos = 2;
+numeroUno = 12;
+numeroDos = 8;
+numeroUno = false;
+numeroDos = true;
+
+//inmutables
+const configuracionArchivo = 'PDF';
+//configuracionArchivos = 'XML';
+//vamos a preferir const >let> nunca var
+
+//T ipos de variables (primitivas)
+const numero = 1; // number
+const sueldo = 1; //number
+const texto = 'Jenny'; //"Adrian string
+const apellidos = 'Llano'; //string
+const boolean = true; //boolean
+const hijos = null; //object
+const zapatos = 1; //undefined
+
+console.log(typeof numero);
+console.log(typeof sueldo);
+console.log(typeof texto);
+console.log(typeof apellidos);
+console.log(typeof boolean);
+console.log(typeof hijos);
+console.log(typeof zapatos);
