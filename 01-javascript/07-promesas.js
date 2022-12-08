5808208ea54cb12e52aa1a11038c932aacd4f1ed
JennyL00 <55715205+JennyL00@users.noreply.github.com>
2022-12-08 17:20:29 -0500

    Create 07-promesas.js

---
+++ 01-javascript/07-promesas.js
@@ -0,0 +1,48 @@
+const fs = require('fs');
+
+function promesasEsPar(numero){// f->devuelve una promesa
+  const miPrimerPromesa = new Promise(
+    (resolve, reject)=>{
+      if(numero%2 === 0){resolve(numero); /*return (then)*/}
+      else{reject(':(no es par');/*throw (catch)*/}
+    }
+  );
+  return miPrimerPromesa
+}
+
+function promesaElevarAlCuadrado(numero){
+  return new Promise((res)=>res(Math.pow(numero,2)));
+}
+
+
+promesaEsPar(numero:4)
+.then(
+  (data)=>{
+    console.log('DATA',data);
+    return promesaElevarAlCuadrado(data);
+  }
+)
+.then(
+  (data)=>{
+    console.log('DATA',data);
+    return promesaElevarAlCuadrado(data);
+  }
+)
+.then(
+  (data)=>{
+    console.log('DATA Final',data);
+    return promesaElevarAlCuadrado(data);
+  }
+)
+.then(//try
+  (data)=>{console.log('DATA',data);}
+)
+.catch( //catch
+  (error)=>{console.error('ERROR', error);}
+)
+.finally( //finally
+  onFinally:()=>{console.log('Finally');}
+);
+
+
+
