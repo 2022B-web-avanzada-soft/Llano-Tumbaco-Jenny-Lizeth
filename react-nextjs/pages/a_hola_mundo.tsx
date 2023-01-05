7054755a7cac56ccc76e7feb31dc7cf1fc0bbdb3
JennyL00 <55715205+JennyL00@users.noreply.github.com>
2023-01-05 12:50:52 -0500

    r

--- react-nextjs/pages/a_hola_mundo.tsx
+++ react-nextjs/pages/a_hola_mundo.tsx
@@ -1,3 +1,13 @@
+/*const a_componente = function(){
+    return (<></>)
+}*/
+/*
+export default a_componente
+const b_componente = () =>{
+    return <></>
+}
+*/
+
 export default function a_hola_mundo(){
     return(
         <>
