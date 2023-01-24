0874c4d3ed493d9761d43701a55a307175bb76e9
JennyL00 <55715205+JennyL00@users.noreply.github.com>
2023-01-24 10:55:45 -0500

    clase validar formularios

--- react-nextjs/pages/h_react_hook_form.tsx
+++ react-nextjs/pages/h_react_hook_form.tsx
@@ -1,17 +1,19 @@
 import {useState} from "react";
-import {useForm} from "react-hook-form";
+import {Controller, useForm} from "react-hook-form";
 import Layout from "../components/Layout";
-import {Button} from "@mui/material";
+import {Button, FormControl, InputLabel,MenuItem,Select} from "@mui/material";
 import {registerStyles} from "@emotion/utils";
 type FormularioEjemplo = {
     nombre:string;
+    estadoCivil: string;
 }
 export default function (){
     const [nombre, setNombre] = useState('Adrian');
-    const {handleSubmit, register} = useForm<FormularioEjemplo>(
+    const {handleSubmit, register, formState:{errors, isValid}, control} = useForm<FormularioEjemplo>(
         {
             defaultValues: {
                 nombre: 'Vicente',
+                estadoCivil: ''
             },
             mode:'all'
         }
@@ -31,13 +33,41 @@ export default function (){
                     className="form-control"
                     placeholder="EJ:Adrian"
                     id="nombre"
-                        {...register('nombre')}
+                        {...register('nombre',{
+                            required: true,
+                            message: 'nombre requerido'
+                        },
+                            maxLength: {value:20, message:'Longitud máxima 20'},
+                            minLength: {value:5, message:'Longitud mínima 5'},
+                            validate:{
+                                soloNumeros: (valorActual) =>{
+                                    //transformar a número un string
+                                    //number("1")
+                                    //+"1"
+                                    if(Number.isNaN+(valorActual)){
+                                        //se puede devolver un false o un error
+                                        //retur false // error
+                                        return 'Ingrese solo números';
+                                    }else{
+                                        //se devuelve un true
+                                        return true; //está correcto
+                                    }
+                                }
+                            }
+                        })}
                     aria-describedby="nombreHelp"/>
                     <div id="nombreHelp" className="form-text">
                         Ingresa tu nombre.
                     </div>
+                    {errors.nombre &&
+                        <div classname="alert alert-warning"
+                            role="alert">
+                            Tiene errores {errors.nombre.message}
+                    }
                 </div>
-                <Button type="submit" variant='outlined'>Submit</Button>
+                <Button type="submit" 
+                    disabled={!isValid}
+                    variant='outlined'>Submit</Button>
             </form>
         </Layout>
         </>)
